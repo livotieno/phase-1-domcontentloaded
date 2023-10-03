d4b3251ab38cef9889595f319ccff97c6f4eb036
livotieno <livio5261@gmail.com>
2023-10-03 19:21:13 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    changeText();
+});
+
+const changeText = () => {
+    const p = document.querySelector("#text");
+    p.textContent = "This is really cool!";
+};
